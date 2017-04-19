b528fddfb4a8f330da405e28fb26ed497893a922
aaron v cohen <avcohen@gmail.com>
2017-04-19 17:25:52 -0400

    passport integration and update routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,43 +1,39 @@
-const express = require('express');
-const auth = express();
 const db = require('sqlite');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 
-// function hashPassword(password, salt) {
-//   var hash = crypto.createHash('sha256');
-//   hash.update(password);
-//   hash.update(salt);
-//   return hash.digest('hex');
-// }
-
 //          user serialization
 // ———————————————————————————————————
-passport.serializeUser(function(user, done) {
-  console.log(' in serialize user')
+passport.serializeUser((user, done) =>  {
+  console.log('SERIALIZED USER');
+  console.log(user)
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  console.log(' in deserialize user')
-  db.get(`SELECT id FROM Users WHERE id IS '${id}'`, (err, row) => {
-    if (!row) return done(null, false);
-    return done(null, row)
-  })
+passport.deserializeUser((id, done) => {
+  db.get(`SELECT id FROM Users WHERE id IS '${id}'`)
+    .then((err, row) => {
+      if (!row) return done(null, false);
+      console.log('DERIALIZED USER');
+      return done(null, row)
+    })
+    .catch(err => console.error(err.stack))
 });
 
 //               user auth
 // ———————————————————————————————————
 passport.use(new LocalStrategy(
   (username, password, done) => {
-    console.log(' in auth strategy')
-    console.log(username)
-    console.log(password)
-    db.get(`SELECT email, password FROM Users WHERE email IS '${username}' AND password IS '${password}'`, (err, row) => {
-      if (!row) return done(null, false);
-      return done(null, row);
-    })
+    console.log('IN AUTH STRATEGY')
+    console.log('USERNAME :' + username)
+    db.get(`SELECT id, email, password FROM Users WHERE email IS '${username}' AND password = '${password}'`)
+      .then((row) => {
+        if (!row) return done(null, false);
+        console.log('LOGIN SUCCESS!!!');
+        return done(null, row);
+      })
+      .catch(err => console.error(err.stack))
   }
 ));
 
--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,7 @@ const express = require('express');
 const login = express.Router();
 const db = require('sqlite')
 const parser = require('body-parser');
-
 const passport = require('../middleware/auth');
-// passport options
-const options = { successRedirect : '/api/users',
-                  failureRedirect : '/bad-login'
-                }
-
 
 // middleware
 login.use(parser.urlencoded({
@@ -16,11 +10,11 @@ login.use(parser.urlencoded({
 }));
 login.use(parser.json());
 
-// base url of route /login
+//     BASE URL OF ROUTE = /login
+// ———————————————————————————————————
 
-// user login route
-login.post('/', passport.authenticate('local', options), (req, res) => {
-
+login.post('/', passport.authenticate('local'), (req, res) => {
+  res.redirect('/home')
   // pull form values from post request
   // const username = req.body.username;
   // const password = req.body.password;
@@ -39,30 +33,37 @@ login.post('/', passport.authenticate('local', options), (req, res) => {
 // user signup route
 login.post('/signup', (req, res) => {
   // future additions :
-    // check to see if username exisits
-    // force passwords to match
+    // DB — check to see if username exisits
+    // FE/BE — force passwords to match
 
-  // pull form values from post request
+  // Pulls values from post request
   const username = req.body.username;
   const password = req.body.password;
   const password2 = req.body.password2;
 
+  // FE — Simple password match check
   if (password !== password2 ) {
     // FE — some front end  logic to signify passwords don't match
     res.redirect('/')
   } else {
-
+  // FIND ALL USERS W USERNAME = FORM DATA USERNAME
   db.all("SELECT * FROM Users WHERE email = ?", username)
     .then((v) => {
+      // CHECK TO SEE IF USER ALREADY EXISTS
       if (v.length > 0){
         // FE - Replace with response to thorw error that a user already exists with this name
         console.log('USER EXISTS')
+        // BAIL AND SEND USER BACK TO HOMEPAGE
         res.redirect('/')
+        // RETURN TO ESCAPE FUNCTION
         return;
       } else {
+        // GET USERS FROM DB
         db.get("SELECT * FROM Users")
           .then(() => {
+            // INSERT FORM DATA INTO DB, RETURN NO VALUES
             db.exec(`INSERT INTO Users (email, password) VALUES ('${username}','${password}')`)
+            // REDIRECT TO HOMEPAGE
             res.redirect('/')
           })
           .catch(err => console.log(err.stack))
