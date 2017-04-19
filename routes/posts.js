a57073036b336cba4b02b75afccacb4c8e83429e
kirsi cebalos <kince0927@gmail.com>
2017-04-18 21:00:26 -0400

    added create post feature + api point for getting all posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,46 @@
 const express = require('express');
 const posts = express.Router();
 const db = require('sqlite');
+const parser = require('body-parser');
+
+// middleware
+posts.use(parser.urlencoded({
+    extended: true
+}));
+posts.use(parser.json());
 
 // get posts by USER ID
-posts.get('/:userid/posts', (req, res) => {
+posts.get('/:userid', (req, res) => {
   // get list of users //
-  db.all('SELECT * FROM Users')
+  const username = req.params.userid
+  db.get(`SELECT id FROM Users WHERE email = ?`, username)
     .then(v => {
       console.log(v)
-      return res.send(v)
+      // res.send(v)
+      return v;
+    })
+    .then(v => {
+      return db.get('SELECT * FROM Posts WHERE user_id = ?', v)
+    })
+    .then(userPosts => {
+      console.log("here!" + userPosts);
     })
     .catch(err => console.log(err.stack))
 });
 
-// create posts by USER ID
-posts.post('/:userid/posts', (req, res) => {
+// create posts from form
+posts.post('/create', (req, res) => {
   // get list of users //
-  db.all('SELECT * FROM Users')
-    .then(v => {
-      console.log(v)
-      return res.send(v)
+  // console.log('here')
+  const user = req.body.userid;
+  const image = req.body.image;
+  const words = req.body.summary;
+
+  console.log( user + " " + image + " " + words)
+
+  db.get("SELECT * FROM Posts")
+    .then(() => {
+      db.exec(`INSERT INTO Posts (user_id, description, url) VALUES ('${user}','${words}','${image}')`)
     })
     .catch(err => console.log(err.stack))
 });
@@ -27,10 +48,11 @@ posts.post('/:userid/posts', (req, res) => {
 // get posts of followed users
 posts.get('/:userid/feed', (req, res) => {
   // get list of users //
-  db.all('SELECT * FROM Users')
+  db.all(`SELECT id * FROM Users`)
     .then(v => {
       console.log(v)
-      return res.send(v)
+      // return res.send(v)
+      return db.get('SELECT name post * FROM Users WHERE name = id and post = posts ', [id.lastid])
     })
     .catch(err => console.log(err.stack))
 });
