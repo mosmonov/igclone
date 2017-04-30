7155122910197e5226872c59b0024855250376f1
Maksat Osmonov <mkstnani08@gmail.com>
2017-04-30 17:10:45 -0400

    got all public js files worked and design restyled

--- public/js/home.js
+++ public/js/home.js
@@ -7,19 +7,35 @@ GET('/')
     const loginForm = document.createElement('div');
     loginForm.classList.add('form-group');
     loginForm.innerHTML =
-    `<form action="/login" method="post" >
-        <div>
-            <label>Username:</label>
-            <input type="text" name="username"/>
-        </div>
-        <div>
-            <label>Password:</label>
-            <input type="password" name="password"/>
-        </div>
-        <div>
-            <input type="submit" value="Log In"/>
-        </div>
-      </form>`
+    `
+        
+            <form action="/login" method="post" >
+                <div class="row">
+                    <div class="two columns"></div>
+                    <div class="eight columns">
+                        <label for="exampleEmailInput"> Username: </label>
+                        <input class="js-name" type="text" name="username" placeholder="Name">
+                    </div> <!--eight columns-->
+                    <div class="two columns"></div>
+                </div> <!--row-->
+                <div class="row">
+                    <div class="two columns"></div>
+                    <div class="eight columns">
+                        <label for="exampleEmailInput"> Password: </label>
+                        <input class="js-pw" type="password" name="password" placeholder="Password">
+                        <div class="js-sign-in">
+                            <input type="submit" value="Log In"/>
+                        </div>
+                    </div> <!--eight columns-->
+                    <div class="two columns"></div>
+                </div> <!--row-->
+            </form>
+        
+  `
+
+
+
+
     loginContainer.appendChild(loginForm)
 
 
@@ -27,7 +43,8 @@ GET('/')
     const signupForm = document.createElement('div');
     signupForm.classList.add('form-group');
     signupForm.innerHTML =
-    `<form action="/signup" method="post" >
+    `
+    <form action="/signup" method="post" >
         <div>
             <label>Username:</label>
             <input type="text" name="username"/>
@@ -43,7 +60,8 @@ GET('/')
         <div>
             <input type="submit" value="Signup"/>
         </div>
-      </form>`
+      </form>
+      `
     signupContainer.appendChild(signupForm)
   })
   .catch(err => console.error(err.stack))
--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,41 @@
 			request.send();
 		});
 	} // GET
+	function POST(url, data) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest();
+			request.open('POST', url);
+			request.setRequestHeader('Content-Type', 'application/json');
+
+			request.onload = () => {
+				const data = JSON.stringify(request.responseText)
+				resolve(data)
+			};
+			request.onerror = (err) => {
+				reject(err)
+			};
+			request.send(JSON.stringify(data));
+		});
+	} // POST
+
+// if(templateName === 'post'){
+// 	const addPost = document.querySelector('.js-add-post');
+// 	  addPost.addEventListener('click', (e) => {
+// 			const input = document.querySelector('.js-post-text').value;
+// 			// input.setAttribute('disabled', 'disabled');
+// 			console.log(input)
+// 			POST('/post/create', {
+// 				description: input.value,
+// 				when: new Date().getTime() + 9 * 60 * 60 * 1000
+// 			}).then((data) => {
+// 				// input.removeAttribute('disabled');
+// 				input.value = '';
+// 				// render(data);
+// 				console.log(data)
+// 			});
+
+// 		});
+// }
 
 
 // reactivate on switch to non html5 form based signup / login
